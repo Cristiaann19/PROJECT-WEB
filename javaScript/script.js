e97aff709af8cc0880b0cf70bdeb6ed9dbd18c84
JuFer007 <juniorfernandozumaetagolac@gmail.com>
2025-07-02 12:03:20 -0500

    lo ultimo

--- javaScript/script.js
+++ javaScript/script.js
@@ -7,11 +7,20 @@ menu.addEventListener('click',()=>{
 });
 
 //Para ocultar el header al hacer scroll
+let lastScrollTop = 0;
+
 window.onscroll = function() {
     var header = document.querySelector('header');
-    if (window.scrollY > 100) {
-        header.classList.add('hidden');
+    let currentScroll = window.scrollY;
+
+    if (currentScroll > 100) {
+        if (currentScroll > lastScrollTop) {
+            header.classList.add('hidden');
+        } else {
+            header.classList.remove('hidden');
+        }
     } else {
         header.classList.remove('hidden');
     }
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
 };
